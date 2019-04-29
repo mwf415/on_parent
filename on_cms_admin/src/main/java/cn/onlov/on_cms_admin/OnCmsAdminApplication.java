4f793a6a3912e22d2eb7203912dd790d1121dadd
mwf415 <mwf415@126.com>
2019-04-29 19:54:07 +0800

    tt

---
+++ on_cms_admin/src/main/java/cn/onlov/on_cms_admin/OnCmsAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.onlov.on_cms_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnCmsAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnCmsAdminApplication.class, args);
+    }
+
+}
---
+++ on_cms_admin/src/test/java/cn/onlov/on_cms_admin/OnCmsAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.onlov.on_cms_admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnCmsAdminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
