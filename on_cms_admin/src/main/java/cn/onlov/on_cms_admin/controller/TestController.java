afacc53670662a6edc3249a6c1c9167ec42bf265
mwf415 <mwf415@126.com>
2019-04-30 11:59:05 +0800

    tj

--- on_cms_admin/src/main/java/cn/onlov/on_cms_admin/controller/TestController.java
+++ on_cms_admin/src/main/java/cn/onlov/on_cms_admin/controller/TestController.java
@@ -1,4 +1,18 @@
 package cn.onlov.on_cms_admin.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
 public class TestController {
+
+    @RequestMapping("/start")
+    public String index() {
+        log.info("log 启动成功");
+        System.out.println("启动成功");
+        return "启动成功";
+    }
+
 }
