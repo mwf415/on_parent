2ef8e29851e3c844ea694361e5801e336ee3a76f
mwf415 <mwf415@126.com>
2019-05-12 17:05:52 +0800

    del

--- on_cms_admin_controller/src/main/java/cn/onlov/cms/admin/filter/HibernateBootConfiguration.java
+++ on_cms_admin_controller/src/main/java/cn/onlov/cms/admin/filter/HibernateBootConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration   //标注此文件为一个配置项，spring boot才会扫描到该配置。
-@ImportResource(locations={"classpath:config/shiro-context.xml"})
-public class ShiroBootConfiguration extends WebMvcConfigurerAdapter {
+@ImportResource(locations={"classpath:config/application-context.xml"})
+public class HibernateBootConfiguration extends WebMvcConfigurerAdapter {
 
 }
